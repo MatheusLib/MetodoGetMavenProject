12d9718d51fec4a42751fe288c5b011a3c993dd1
Matheus Libanio <matheus.alexandre.libanio@gmail.com>
2024-01-10 20:33:58 -0300

    EX01

--- src/main/java/com/generation/olamundo/controller/OlaMundoController.java
+++ src/main/java/com/generation/olamundo/controller/OlaMundoController.java
@@ -1,15 +1,32 @@
 package com.generation.olamundo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Olamundo")
+
 public class OlaMundoController {
 	@GetMapping
 	public String OlaMundo() {
 		return "Ola Mundo na familia 71";
+	}
+	@GetMapping("/bsm")
+	public ArrayList ListaBSM() {
+		ArrayList<String> bsm = new ArrayList<String>();
+		bsm.add("comunicação");
+		bsm.add("proatividade");
+		bsm.add("orientação ao detalhe");
+		bsm.add("trabalho em equipe");
+		bsm.add("mentalidade de crescimento");
+		bsm.add("orientação ao futuro");
+		bsm.add("responsabilidade pessoal");
+		bsm.add("persistência");
+
+		return bsm;
 	}
 
 }
